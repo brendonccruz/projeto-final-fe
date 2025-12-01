55574064779d65ca63043c2b6695fc02d37e8dd6
brendonccruz <b.araujodacruz@gmail.com>
2025-12-01 13:57:22 -0300

    Ajustes simples

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
     const contactForm = document.getElementById('contactForm');
     
-    // Verifica se o formulário existe na página atual antes de rodar o código
     if (contactForm) {
         
         contactForm.addEventListener('submit', function(event) {
-            event.preventDefault(); // 1. Impede o envio real e o recarregamento da página
-
-            // Aqui você poderia adicionar validações extras se quisesse
-            // Mas o atributo 'required' do HTML já faz o trabalho pesado
+            event.preventDefault(); 
             
-            // 2. Pega os valores (Simulação de envio)
             const nome = document.getElementById('nome').value;
             console.log("Enviando mensagem de: " + nome);
 
-            // 3. Efeito Visual: Esconde o Form e Mostra o Sucesso
-            contactForm.style.display = 'none'; // Some com o formulário
+            contactForm.style.display = 'none';
             
             const feedback = document.getElementById('feedback-success');
-            feedback.classList.remove('d-none'); // Remove a classe que esconde
+            feedback.classList.remove('d-none'); 
         });
     }
